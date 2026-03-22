eec0ab857b3fed79ea44b238e24197aec87b3b56
mm7648-Mahasvin-Varma <mm7648@srmist.edu.in>
2026-03-22 18:17:16 +0530

    Coded UC2 to Print OOPS to Console

--- src/OOPSBannerApp.java
+++ src/OOPSBannerApp.java
@@ -4,7 +4,7 @@
  * This class demonstrates a simple Java application that displays the Object
  * Oriented Programming System (OOPS) acronym to the console.
  *
- * @author Akshdeep Singh
+ * @author MUDUNURI.MAHASVINVARMA
  * @version 2.0
  */
 public class OOPSBannerApp {
